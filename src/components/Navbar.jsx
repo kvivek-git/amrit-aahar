a096f8565f5e8a5bb5bc381de49a39116182a6b5
Vivek Kumar <contact.vivek1009@gmail.com>
2025-06-10 11:55:45 +0530

    minor spelling correction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
       <nav className="flex justify-between items-center p-4 bg-[#FFFFFF] text-[#5E412E] shadow-md">
         <img src="/icons/menu.png" alt="menu" className="h-10 cursor-pointer" />
-        <h1 className="font-bold text-2xl mx-2">Opent Label Nutrition</h1>
+        <h1 className="font-bold text-2xl mx-2">Open Label Nutrition</h1>
         <div className="flex items-center gap-4">
           
           <img src="/icons/cart.png" alt="menu" className="h-10 cursor-pointer" />
